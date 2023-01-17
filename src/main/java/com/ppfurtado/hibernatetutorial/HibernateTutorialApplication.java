dc05303aedd42297991b0f5f788b12d26e472df2
Pedro Furtado <pedro.nazare@pdcase.com.br>
2023-01-17 18:05:06 -0300

    initial commit

---
+++ src/main/java/com/ppfurtado/hibernatetutorial/HibernateTutorialApplication.java
@@ -0,0 +1,13 @@
+package com.ppfurtado.hibernatetutorial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HibernateTutorialApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HibernateTutorialApplication.class, args);
+    }
+
+}
