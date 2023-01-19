a08d6fb61802627d34bdd4ad05be9d4134b71aa3
Pedro Furtado <pedro.nazare@pdcase.com.br>
2023-01-19 14:07:55 -0300

    feat: anotação repository

--- src/main/java/com/ppfurtado/hibernatetutorial/domain/repositories/IngredientesRepository.java
+++ src/main/java/com/ppfurtado/hibernatetutorial/domain/repositories/IngredientesRepository.java
@@ -2,6 +2,8 @@ package com.ppfurtado.hibernatetutorial.domain.repositories;
 
 import com.ppfurtado.hibernatetutorial.domain.model.Ingredientes;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface IngredientesRepository extends JpaRepository<Ingredientes, Long> {
 }
