b4d009292f793d68e977415cb09edbfa45619f85
Pedro Furtado <pedro.nazare@pdcase.com.br>
2023-01-20 15:20:25 -0300

    feat: anotação para repository

--- src/main/java/com/ppfurtado/hibernatetutorial/domain/repositories/FichaTecnicaRepository.java
+++ src/main/java/com/ppfurtado/hibernatetutorial/domain/repositories/FichaTecnicaRepository.java
@@ -2,6 +2,9 @@ package com.ppfurtado.hibernatetutorial.domain.repositories;
 
 import com.ppfurtado.hibernatetutorial.domain.model.FichaTecnica;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+
+@Repository
 public interface FichaTecnicaRepository extends JpaRepository<FichaTecnica, Long> {
 }
