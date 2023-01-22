e94bc041a40bccbb0d465d61c8d950f69d7c877c
Pedro Furtado <furtado.pedropaulo@gmail.com>
2023-01-22 00:36:03 -0300

    refact: criando métodos para mapear id para entidade e entidade para id

--- src/main/java/com/ppfurtado/hibernatetutorial/domain/mappers/FichaTecnicaMapper.java
+++ src/main/java/com/ppfurtado/hibernatetutorial/domain/mappers/FichaTecnicaMapper.java
@@ -2,8 +2,10 @@ package com.ppfurtado.hibernatetutorial.domain.mappers;
 
 import com.ppfurtado.hibernatetutorial.domain.dtos.FichaTecnicaRequest;
 import com.ppfurtado.hibernatetutorial.domain.model.FichaTecnica;
+import com.ppfurtado.hibernatetutorial.domain.model.Ingredientes;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
+import org.mapstruct.Named;
 import org.mapstruct.factory.Mappers;
 
 @Mapper
@@ -11,8 +13,28 @@ public interface FichaTecnicaMapper {
 
     FichaTecnicaMapper INSTANCE = Mappers.getMapper(FichaTecnicaMapper.class);
 
+    @Mapping(target = "ingredienteId", source = "ingredienteId", qualifiedByName = "idToEntity" )
     FichaTecnica toEntity(FichaTecnicaRequest fichaTecnicaRequest);
 
+    @Mapping(target = "ingredienteId", source = "ingredienteId", qualifiedByName = "entityToId" )
     FichaTecnicaRequest toRequest(FichaTecnica fichaTecnica);
+
+    @Named("idToEntity")
+    static Ingredientes idToEntity(Long ingredienteId){
+        Ingredientes ingrediente = new Ingredientes();
+        if (ingredienteId == null) {
+            return null;
+        }
+        ingrediente.setId(ingredienteId);
+        return ingrediente;
+    }
+
+    @Named("entityToId")
+    static Long entityToId(Ingredientes ingrediente){
+        if (ingrediente == null){
+            return  null;
+        }
+        return ingrediente.getId();
+    }
 
 }
