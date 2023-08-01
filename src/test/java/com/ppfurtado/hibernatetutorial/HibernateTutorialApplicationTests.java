48216182b16d03f43be6b213e646b5d414b341e0
Pedro Furtado <furtado.pedropaulo@gmail.com>
2023-08-01 12:24:30 -0300

    remove: excluindo test padrão

--- src/test/java/com/ppfurtado/hibernatetutorial/HibernateTutorialApplicationTests.java
+++ src/test/java/com/ppfurtado/hibernatetutorial/HibernateTutorialApplicationTests.java
@@ -3,10 +3,10 @@ package com.ppfurtado.hibernatetutorial;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+
 class HibernateTutorialApplicationTests {
 
-    @Test
+
     void contextLoads() {
     }
 
