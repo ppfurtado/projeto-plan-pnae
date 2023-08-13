312f8a8887027bb64d20e45705a9f3439988be00
Pedro Furtado <furtado.pedropaulo@gmail.com>
2023-08-13 17:19:28 -0300

    refactor: recompilacao dos metadados #1 closed

--- src/main/java/com/ppfurtado/planpnae/exception/IngredienteNotFoundException.java
+++ src/main/java/com/ppfurtado/planpnae/exception/IngredienteNotFoundException.java
@@ -3,6 +3,6 @@ package com.ppfurtado.planpnae.exception;
 public class IngredienteNotFoundException extends RuntimeException {
 
     public IngredienteNotFoundException(Long id) {
-        super(String.format("Ingrediente não encontrado id %d", id));
+        super(String.format("Ingrediente com id %d não encontrado", id));
     }
 }
